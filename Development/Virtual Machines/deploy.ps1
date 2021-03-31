d96f5ad9066d0003cc71aaab67724ae8cbfa5a69
dennischavez789 <dennischavez789@gmail.com>
2021-03-31 14:53:23 -0500

    Updating

--- Development/Virtual Machines/deploy.ps1
+++ Development/Virtual Machines/deploy.ps1
@@ -1,6 +1,6 @@
 $rg = 'Development'
 $deploymentName = 'dc'
-$parameterFile = 'C:\Users\dechavez\OneDrive - Microsoft\Desktop\Programming\Microsoft\AIRS\VirtualMachines\Parameters\parameters.json'
+$parameterFile = 'C:\Users\dechavez\OneDrive - Microsoft\Desktop\Work\Git\dmc2312.com\Development\Virtual Machines\Parameters\parameters.json'
 New-AzResourceGroupDeployment `
     -Name $deploymentname `
     -ResourceGroupName $rg `
